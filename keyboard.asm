502f11bb2e80d81ccca466f597337cdb9fb9df13
Josh <josh.joseph@mail.utoronto.ca>
2024-03-24 16:00:18 -0400

    added rotation for Z piece

--- keyboard.asm
+++ keyboard.asm
@@ -1,5 +1,6 @@
 # .include "shared_data.asm"
 
+
 ##############################################################################
 # Immutable Data
 ##############################################################################
@@ -35,6 +36,7 @@ handleKeyboardInput:
         beq $t1, 0x61, respond_to_A     # Check if the key 'A' was pressed
         beq $t1, 0x73, respond_to_S     # Check if the key 'S' was pressed
         beq $t1, 0x64, respond_to_D     # Check if the key 'D' was pressed
+        beq $t1, 0x77, respond_to_W     # Check if the key 'W' was pressed 
         j handleKeyboardInput_exit
         
         respond_to_S:
@@ -62,10 +64,133 @@ handleKeyboardInput:
     
             j handleKeyboardInput_exit
             
+        respond_to_W:
+            addi $s4, $zero, 3       #Update direction flag to 3(up/rotate)
+            
+            jal rotate_Z
+            
+            j handleKeyboardInput_exit
+            
     handleKeyboardInput_exit:
     jr $ra                  # Return to caller
 
 
+
+   # Function to make Z piece tetromino rotate 90 degrees clockwise
+rotate_Z:
+    la $s0, tetromino       # $s0 = tetromino array starting address
+    lw $s1, cols            # $s1 = number of rows
+    la $s2, ADDR_DSPL       # Starting address of bitmap display
+    la $s3, array           # Game array starting address
+    
+    lw $t1, ($s0)       # $t1 = address of current tetromino cell
+        
+    #addi $s0, $s0, 4                        # Increment array address by 4 bytes
+    
+    # Calculate address ($t2) of tetromino cell in game array 
+    add $t2, $zero, $t1                 # Set $t2 equal to address of tetromino cell in display
+    sub $t2, $t2, $s0                   # Subtract by the starting address of the display to find offset
+    add $t2, $t2, $s2                   # Add the starting address of the game array
+        
+    # Find address of the cell right of the tetromino in game array
+    addi $t4, $t2, 4   # Add offset to current cell address
+            
+    # Find colour stored at address in game array
+    lw $t3, ($t4)
+    
+    addi $t0, $zero, 0      # $t0 = loop counter
+    
+    bne $t3, 0x0, rotate_Z_from_horizontal_loop    # Detect if the colour does not equal black 
+    j rotate_Z_from_vertical_loop                  # rotate from vertical orientation of right cell of first tetromino cell is black(empty)
+    
+    
+    rotate_Z_from_horizontal_loop:
+        beq $t0, 4, moveTetromino_end_loops      # End loop if array location reaches the end
+        
+        # Print black to the previous tetromino cell
+        lw $t1, ($s0)           # Obtain the address of the current tetromino cell
+        add $s2, $t1, $zero     # Obtain address of position of cell in bitmap display
+        sw $zero, ($s2)         # Print black to bitmap display
+        
+        beq $t0, $zero, shift_first   # If $t0 = 0, shift the first tetromino element
+        beq $t0, 1, shift_second      # If $t0 = 1, shift the second tetromino element
+        beq $t0, 2, shift_third    # If $t0 = 2, shift the third tetromino element
+        beq $t0, 3, shift_last     # If $t0 = 3, shift the fourth tetromino element
+        j end_shift
+        
+        shift_first:
+            addi $t1 $t1, 8        # Add offset to the address
+            sw $t1, ($s0)           # Load address back into the array
+            j end_shift
+        shift_second:
+            mult $t2, $s1, 4        # Number of columns * 4 = the offset of the address to shift dowm
+            add $t1, $t1, $t2       # Add offset to the address
+            addi $t1, $t1, 4        # Add addtional 4 to the address(move one cell right)
+            sw $t1, ($s0)           # Load address back into the array
+            j end_shift
+        shift_third:
+            add $t1, $t1, $zero    # No offset needed in this case
+            sw $t1, ($s0)           # Load address back into the array
+            j end_shift
+        shift_last:
+            mult $t2, $s1, 4        # Number of columns * 4 = the offset of the address to shift dowm
+            add $t1, $t1, $t2       # Add offset to the address
+            addi $t1, $t1, -4       #Subtract 4 from the address(move one cell left)
+            sw $t1, ($s0)           # Load address back into the array
+            j end_shift
+            
+        end_shift:
+        addi $s0, $s0, 4            # Move to next address in memory
+        addi $t0, $t0, 1            # Increment loop counter by 1
+        j rotate_Z_from_horizontal_loop
+    
+    moveTetromino_end_loops:
+    addi $s4, $zero, -1     # Reset the movement direction
+    jr $ra
+    
+    rotate_Z_from_vertical_loop:
+        beq $t0, 4, moveTetromino_end_loo    # End loop if array location reaches the end
+        
+        # Print black to the previous tetromino cell
+        lw $t1, ($s0)           # Obtain the address of the current tetromino cell
+        add $s2, $t1, $zero     # Obtain address of position of cell in bitmap display
+        sw $zero, ($s2)         # Print black to bitmap display
+        
+        beq $t0, $zero, shift_first   # If $t0 = 0, shift the first tetromino element
+        beq $t0, 1, shift_second      # If $t0 = 1, shift the second tetromino element
+        beq $t0, 2, shift_third    # If $t0 = 2, shift the third tetromino element
+        beq $t0, 3, shift_last     # If $t0 = 3, shift the fourth tetromino element
+        j end_shift1
+        
+        shift_first:
+            addi $t1, $t1, -8       # Add offset to the address
+            sw $t1, ($s0)           # Load address back into the array
+            j end_shift1
+        shift_second:
+            mult $t2, $s1, -4        # Number of columns * -4 = the offset of the address to shift up
+            add $t1, $t1, $t2       # Add offset to the address
+            addi $t1, $t1, -4        # Add addtional -4 to the address(move one cell left)
+            sw $t1, ($s0)           # Load address back into the array
+            j end_shift1
+        shift_third:
+           add $t1, $t1, $zero    # No offset needed in this case
+            sw $t1, ($s0)           # Load address back into the array
+            j end_shift1
+        shift_last:
+            mult $t2, $s1, -4        # Number of columns * -4 = the offset of the address to shift dowm
+            add $t1, $t1, $t2       # Add offset to the address
+            addi $t1, $t1, 4       #Add 4 to the address(move one cell right)
+            sw $t1, ($s0)           # Load address back into the array
+            j end_shift1
+            
+        end_shift1:
+        addi $s0, $s0, 4            # Move to next address in memory
+        addi $t0, $t0, 1            # Increment loop counter by 1
+        j rotate_Z_from_vertical_loop
+    
+    moveTetromino_end_loo:
+    addi $s4, $zero, -1     # Reset the movement direction
+    j handleKeyboardInput_exit
 
 # Function to make tetromino move in a given direction
 moveTetromino:
